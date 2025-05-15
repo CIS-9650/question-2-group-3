b288129b400669fa258dabec953760fdf49d7415
Angela Pittman <ANGELA.PITTMAN@baruchmail.cuny.edu>
2025-05-14 21:48:42 -0400

    updating the libraries

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ from urllib.parse import quote
 from bs4 import BeautifulSoup
 import time
 import sqlite3
+import certifi
+import ssl
+
+# Create a custom SSL context using certifi's CA bundle
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+ssl._create_default_https_context = lambda: ssl_context  
+
+
